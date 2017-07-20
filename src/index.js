95c085f09d04544efaeed9e2d291ac760b64fa90
John Edward <john.escuyos@98labs.com>
2017-07-20 19:22:12 +0800

    optional logic, added component-base static store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 /* eslint-disable no-param-reassign */
-function component({ logic, template, styles }) {
-  if (!logic) throw new Error('logic is not defined');
+function component({ logic, template, styles, store = {} }) {
   if (!template) throw new Error('template is not defined');
 
-  logic.prototype.render = (function () {
-    return template.bind(this)({ ...this.props, ...styles });
-  });
+  if (logic) {
+    // 
+    if (store) {
+      logic.prototype.store = store;
+      logic.prototype.setStore = (function (obj = {}) {
+        Object.keys(obj).forEach((key) => {
+          this.store[key] = obj[key];
+          this.forceUpdate();
+        });
+      });
+    }
+
+    logic.prototype.render = (function () {
+      return template.bind(this)({ ...this.props, ...styles });
+    });
+  } else {
+    template.prototype.store = store;
+    return template;
+  }
+
   return logic;
 }
 
